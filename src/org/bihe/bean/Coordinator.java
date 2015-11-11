ebe114443b74967126225aff710e23666606b0ec
Salah <Salah@DESKTOP-6C2V3E0>
2015-11-12 01:15:54 +0330

    Add Demand

---
+++ src/org/bihe/bean/Coordinator.java
@@ -0,0 +1,14 @@
+package org.bihe.bean;
+
+public class Coordinator implements Runnable{
+
+	private Queue 
+	
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/org/bihe/bean/Demand.java
@@ -0,0 +1,38 @@
+package org.bihe.bean;
+
+import java.util.HashMap;
+
+public class Demand {
+
+	private String ID;
+	private HashMap<CarID, Integer> listOfCar;
+	
+	public Demand() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getID() {
+		return ID;
+	}
+
+	public void setID(String iD) {
+		ID = iD;
+	}
+
+	public HashMap<CarID, Integer> getListOfCar() {
+		return listOfCar;
+	}
+
+	public void setListOfCar(HashMap<CarID, Integer> listOfCar) {
+		this.listOfCar = listOfCar;
+	}
+	
+	
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+	
+}
