3e56c3eb4c2dac72bd4feaa511e1c9c5f747a887
siavash.misaghi <siavash.misaghi@Siavash>
2015-11-12 00:50:58 +0330

    producer 1

---
+++ src/org/bihe/bean/Producer.java
@@ -0,0 +1,49 @@
+package org.bihe.bean;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Producer {
+	
+	//-------------------------------------------------
+	//-------------Instance Fields---------------------
+	private ArrayList<ProductionLine> productionLines;
+	private ArrayList<Car> stockCars;
+	private int stockSize;
+	private int freeSpace;
+	private HashMap<String, Integer> cars;
+	
+	//-------------------------------------------------
+	//--------------Constructors-----------------------
+	
+	
+	//-------------------------------------------------
+	//----------------Methods--------------------------
+	public void addProductLine(ProductionLine productionLine){
+		productionLines.add(productionLine);
+	}
+	//-------------------------------------------------
+	private void countCar(){
+		//TODO arayeyey mashin ha ra gerefte va bar asase model, shomaresh 
+		// va dakhele hashmap gharar midahad
+	}
+	//-------------------------------------------------
+	public void addCarToStock(Car car){
+		//TODO add produced car to stock
+	}
+	//-------------------------------------------------
+	public Demand sendAlarmToCoordinator(){
+		//TODO 
+	}
+	//-------------------------------------------------
+	public Demand moveToWareHouse(){
+		//TODO
+	}
+	//-------------------------------------------------
+	public Demand sendDirectlyToSalesMan(){
+		//TODO
+	}
+	//-------------------------------------------------
+	
+
+}
