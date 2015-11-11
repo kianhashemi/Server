a3f2d7f5a76e0955aead4d8f20d109b3c0352a56
Kian <kian.hashemi9@gmail.com>
2015-11-12 01:00:44 +0330

    Add Car & Person Class Add CarID Enum

--- src/org/bihe/bean/Car.java
+++ src/org/bihe/bean/Car.java
@@ -1,5 +1,55 @@
 package org.bihe.bean;
 
 public class Car {
+	// -------------------------------------------------------------
+	// ------------------Instance Fields----------------------------
+	private String color;
+	private String model;
+	private String cost;
+	private String id;
+
+	// -------------------------------------------------------------
+	// ------------------Constructor--------------------------------
+	public Car(String color, String model, String cost, String id) {
+		super();
+		this.color = color;
+		this.model = model;
+		this.cost = cost;
+		this.id = id;
+	}
+
+	// -------------------------------------------------------------
+	// ------------------Accessories----------------------------
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getCost() {
+		return cost;
+	}
+
+	public void setCost(String cost) {
+		this.cost = cost;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
 
 }
---
+++ src/org/bihe/bean/CarID.java
@@ -0,0 +1,8 @@
+package org.bihe.bean;
+
+public enum CarID {
+	// -------------------------------------------------------------
+	// ------------------Model & Color of Cars----------------------
+	BMWBlack, BMWWhite, BMWRed, BenzBlack, BenzWhite, BenzGreen, PeugeotWhite, 
+	PeugeotBlack, PeugeotGray, PeugeotRed
+}
